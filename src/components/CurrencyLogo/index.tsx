e31c3080405af1c398f3bfa5aab0e26298fa2c2a
DuyThao <duythao@bacoor.co>
2021-05-05 14:05:37 +0700

    Update

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -1,4 +1,4 @@
-import { Currency, ETHER, Token } from '@uniswap/sdk'
+import { Currency, ETHER, Token } from 'pawsswap-sdk'
 import React, { useMemo } from 'react'
 import styled from 'styled-components'
 
