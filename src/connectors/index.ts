b06edbc907a276e77405d8245f240c0b28a3c2c4
DuyThao <duythao@bacoor.co>
2021-05-05 15:56:17 +0700

    Temporarily support BSC Testnet

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -29,7 +29,8 @@ export function getNetworkLibrary(): Web3Provider {
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42, 56, 97]
+  // supportedChainIds: [1, 3, 4, 5, 42, 56, 97]
+  supportedChainIds: [97]
 })
 
 // mainnet only
